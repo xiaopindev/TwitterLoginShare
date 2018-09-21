51a9ee2c5a52467d0580b8c9b7f19a9c987031dc
Xiaopin <251089096@qq.com>
2018-09-21 15:24:23 +0800

    原生TwitterKit v3.4.0登录分享

---
+++ TwitterLoginShareExample/Pods/Target Support Files/Pods-TwitterLoginShareExample/Pods-TwitterLoginShareExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TwitterLoginShareExample : NSObject
+@end
+@implementation PodsDummy_Pods_TwitterLoginShareExample
+@end
---
+++ TwitterLoginShareExample/TwitterLoginShareExample/ViewController.m
@@ -0,0 +1,254 @@
+//
+//  ViewController.m
+//  FacebookLoginShareExample
+//
+//  Created by cdmac on 16/4/24.
+//  Copyright © 2016年 chinadailyhk. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import <FBSDKShareKit/FBSDKShareKit.h>
+#import <UIImageView+WebCache.h>
+#import <TwitterKit/TWTRKit.h>
+
+#define kFBSDK NO
+
+@interface ViewController ()<FBSDKSharingDelegate>
+{
+    FBSDKLoginManager *_loginManager;
+    NSString *_userID;
+    NSString *_userName;
+    NSString *_userHeadUrl;
+}
+
+@property (strong, nonatomic) UIButton *btnFacebook;
+@property (weak, nonatomic) IBOutlet UIImageView *userHead;
+@property (weak, nonatomic) IBOutlet UILabel *labID;
+@property (weak, nonatomic) IBOutlet UILabel *labName;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    if(kFBSDK){
+        FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
+        loginButton.center = self.view.center;
+        [self.view addSubview:loginButton];
+    }else{
+        TWTRLogInButton *logInButton = [TWTRLogInButton buttonWithLogInCompletion:^(TWTRSession *session, NSError *error) {
+            if (session) {
+                NSLog(@"signed in as userId:%@ ,userName %@", [session userID],[session userName]);
+                _userID = [session userID];
+                _userName = [session userName];
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    TWTRAPIClient *client = [[TWTRAPIClient alloc] init];
+                    [client loadUserWithID:[session userID] completion:^(TWTRUser *user, NSError *error) {
+                        // handle the response or error
+                        _userHeadUrl = user.profileImageLargeURL;
+                    }];
+                });
+            } else {
+                NSLog(@"error: %@", [error localizedDescription]);
+            }
+        }];
+        logInButton.center = self.view.center;
+        [self.view addSubview:logInButton];
+    }
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)customLogin:(id)sender {
+    if(kFBSDK){
+        FBSDKLoginManager *login = [[FBSDKLoginManager alloc] init];
+        [login logInWithReadPermissions: @[@"public_profile"]
+                     fromViewController:self
+                                handler:^(FBSDKLoginManagerLoginResult *result, NSError *error) {
+                                    if (error) {
+                                        NSLog(@"Process error");
+                                    } else if (result.isCancelled) {
+                                        NSLog(@"Cancelled");
+                                    } else {
+                                        NSLog(@"Logged in");
+                                        //获取用户id, 昵称，大头像
+                                        if ([FBSDKAccessToken currentAccessToken]) {
+                                            if (![[FBSDKAccessToken currentAccessToken].userID isEqualToString:_userID]) {
+                                                FBSDKGraphRequest *request = [[FBSDKGraphRequest alloc] initWithGraphPath:@"me?fields=id,name" parameters:nil];
+                                                [request startWithCompletionHandler:^(FBSDKGraphRequestConnection *connection, id result, NSError *error) {
+                                                    NSLog(@"result\n%@",result);
+                                                    NSString *userID = result[@"id"];
+                                                    
+                                                    if (!error && [[FBSDKAccessToken currentAccessToken].userID isEqualToString:userID]) {
+                                                        _userName = result[@"name"];
+                                                        _userID = userID;
+                                                        _userHeadUrl = [NSString stringWithFormat:@"http://graph.facebook.com/%@/picture?type=large",userID];
+                                                        //具体参见：
+                                                        //https://developers.facebook.com/docs/graph-api/reference/user/picture/
+                                                        //type参数：small,normal,album,large,square
+                                                    }
+                                                }];
+                                            }
+                                        }
+                                        
+                                    }
+                                }];
+    }else{
+        //Twitter登录
+        [[Twitter sharedInstance] logInWithCompletion:^(TWTRSession *session, NSError *error) {
+            if (session) {
+                NSLog(@"signed in as userId:%@ ,userName %@", [session userID],[session userName]);
+                _userID = [session userID];
+                _userName = [session userName];
+                TWTRAPIClient *client = [[TWTRAPIClient alloc] init];
+                [client loadUserWithID:[session userID] completion:^(TWTRUser *user, NSError *error) {
+                    // handle the response or error
+                    //formattedScreenName :@app_cdhk
+                    //user.name : CDHK_APP
+                    //user.screenName : app_cdhk
+                    //https://pbs.twimg.com/profile_images/1025264164053245953/qq0I3dqu_reasonably_small.jpg
+                    _userHeadUrl = user.profileImageLargeURL;
+                }];
+                
+            } else {
+                NSLog(@"error: %@", [error localizedDescription]);
+            }
+        }];
+    }
+    
+}
+
+- (IBAction)getUserInfo:(id)sender {
+    self.labID.text = _userID;
+    self.labName.text = _userName;
+    [self.userHead sd_setImageWithURL:[NSURL URLWithString:_userHeadUrl]];
+}
+
+- (IBAction)logout:(id)sender {
+    if(kFBSDK){
+        [[[FBSDKLoginManager alloc] init] logOut];
+    }else{
+        if([Twitter sharedInstance].sessionStore.hasLoggedInUsers){
+            NSString *userId = [Twitter sharedInstance].sessionStore.session.userID;
+            [[Twitter sharedInstance].sessionStore logOutUserID:userId];
+        }
+    }
+}
+
+- (IBAction)shareContent:(id)sender {
+    NSString *shareTitle = @"This is a Title";
+    NSString *shareText = @"This is a text";
+    //NSString *shareImageUrl = @"https://api.cdeclips.com/vdocms/videoImg/15330259017007413.jpg";
+    //NSString *shareLinkUrl = @"https://www.vdoenglish.com/video/videoView_videoView?video.id=1116";
+    NSString *shareImageUrl = @"https://www.chinadailyhk.com/attachments/image/164/209/125/400933_112283/400933_112283_956_500_jpg.jpg";
+    NSString *shareLinkUrl = @"https://www.chinadailyasia.com/articles/126/230/9/1533261962100.html";
+   
+    
+    if(kFBSDK){
+        //4.35.0
+        /*
+         分享链接：contentURL
+         应用说明：quote （可以放置标题）
+         */
+        
+#if 1
+        FBSDKShareLinkContent *content = [[FBSDKShareLinkContent alloc] init];
+        content.contentURL = [NSURL URLWithString:shareLinkUrl];
+        //引用说明
+        content.quote = [NSString stringWithFormat:@"%@. Click the hyperlink below for details>>",shareTitle];
+        //话题标签
+        content.hashtag = [FBSDKHashtag hashtagWithString:@"#VDOEnglish"];
+        //Facebook 应用
+        [FBSDKShareDialog showFromViewController:self
+                                     withContent:content
+                                        delegate:self];
+#else
+        //messenger 应用
+        //判断messenger 是否安装的方式
+        if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"fb-messenger-share-api://"]]) {
+            //-canOpenURL: failed for URL: "fb-messenger-share-api:/" - error: "The operation couldn’t be completed. (OSStatus error -10814.)"
+            FBSDKShareMessengerGenericTemplateContent *content = [[FBSDKShareMessengerGenericTemplateContent alloc] init];
+            FBSDKShareMessengerGenericTemplateElement *element = [[FBSDKShareMessengerGenericTemplateElement alloc] init];
+            element.title = shareTitle;
+            element.subtitle = shareText;
+            element.imageURL = [NSURL URLWithString:shareImageUrl];
+            
+            FBSDKShareMessengerURLActionButton *detailButton = [[FBSDKShareMessengerURLActionButton alloc] init];
+            detailButton.title = @"View Details";
+            detailButton.url = [NSURL URLWithString:shareLinkUrl];
+            
+            element.defaultAction = detailButton;
+            content.pageID = shareLinkUrl;
+            content.element = element;
+            
+            FBSDKMessageDialog *messageDialog = [[FBSDKMessageDialog alloc] init];
+            messageDialog.shareContent = content;
+            
+            if ([messageDialog canShow]) {
+                [messageDialog show];
+            }
+        }else{
+            NSLog(@"没有安装messenger");
+        }
+        
+#endif
+    }else{
+        //Twitter分享
+#if 0
+        //空的
+        TWTRComposerViewController *composer = [TWTRComposerViewController emptyComposer];
+        [self presentViewController:composer animated:YES completion:nil];
+     
+#else
+        
+        //调用方法
+        TWTRComposer *composer = [[TWTRComposer alloc] init];
+        //设置分享的标题
+        [composer setText:shareTitle];
+        //设置分享的URL
+        [composer setURL:[NSURL URLWithString:shareLinkUrl]];
+        //添加图片, 这里分享的图片传值是 UIimage类型
+        UIImage *shareImage = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:shareImageUrl]]];
+        [composer setImage:shareImage];
+        
+        // Called from a UIViewController
+        if([[TWTRTwitter sharedInstance].sessionStore hasLoggedInUsers]){
+            [composer showFromViewController:self completion:^(TWTRComposerResult result) {
+                if (result == TWTRComposerResultCancelled) {
+                    NSLog(@"Tweet composition cancelled");
+                    
+                }else {
+                    
+                    NSLog(@"Sending Tweet!");
+                }
+            }];
+        }else{
+             NSLog(@"没有登录授权用户");
+        }
+#endif
+    }
+    
+}
+
+-(void)sharer:(id<FBSDKSharing>)sharer didCompleteWithResults:(NSDictionary *)results{
+    NSLog(@"share OK\n%@",results);
+    
+    //如果存在postId,则说明成功
+}
+
+-(void)sharer:(id<FBSDKSharing>)sharer didFailWithError:(NSError *)error{
+    NSLog(@"share faild\n%@",error);
+}
+
+-(void)sharerDidCancel:(id<FBSDKSharing>)sharer{
+    NSLog(@"share Cancel");
+}
+
+@end
